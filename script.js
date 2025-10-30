481d49fd0981b18b1a347b25a1b95df232d38012
JustusKu <asembopunga@gmail.com>
2025-10-30 19:32:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let students = parseInt(prompt("Number of students"));
+let studentName = prompt("Student name");
+let marks = parseFloat(prompt("Student marks(1-100)%"));
+
+console.log("No. of Students:" + students);
+console.log("Name:" + studentName);
+console.log("Marks:" + marks + "%");
+
+
